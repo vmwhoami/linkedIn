26371064117fdbb3dd4acf08bce054796abdb589
vmwhoami <vmwhoami@gmail.com>
2021-02-19 18:38:49 +0200

    add built in timeout function

--- src/selectCountry.js
+++ src/selectCountry.js
@@ -6,22 +6,11 @@ const selectCountry = async (page, country) => {
   await page.waitForSelector("[aria-label='Add a location']");
   await page.click("[aria-label='Add a location']");
   await page.keyboard.type(country, { delay: 500 });
+  // await page.waitForTimeout(4000)
   await page.keyboard.press('ArrowDown', { delay: 100 });
+  await page.waitForSelector(".peek-carousel__slides li:nth-child(4) [data-control-name='filter_show_results']")
   await page.click(".peek-carousel__slides li:nth-child(4) [data-control-name='filter_show_results']")
-  // setTimeout(async () => {
 
-  //   setTimeout(async () => {
-
-  //     await page.keyboard.press('Enter');
-  //     await page.click(".peek-carousel__slides li:nth-child(4) [data-control-name='filter_show_results']")
-  //   })
-
-
-  // }, 6000);
-
-  // await page.keyboard.press('ArrowLeft');
-  // await page.keyboard.press('ArrowLeft');
-  // await page.keyboard.press('ArrowDown');
 
   await page.click(".peek-carousel__slides li:nth-child(4) [data-control-name='filter_show_results']")
 }
