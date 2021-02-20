bbf44b6918c664e82d593a8191ae5fe858327324
vmwhoami <vmwhoami@gmail.com>
2021-02-21 01:55:29 +0200

    add a connections

--- src/index.js
+++ src/index.js
@@ -6,47 +6,7 @@ import { startBrowser, closeBrowser } from './startCloseBrowser.js';
 import selectCountry from './selectCountry.js'
 
 
-function getText(linkText) {
-  linkText = linkText.replace(/\r\n|\r/g, "\n");
-  linkText = linkText.replace(/\ +/g, " ");
 
-  // Replace &nbsp; with a space 
-  var nbspPattern = new RegExp(String.fromCharCode(160), "g");
-  return linkText.replace(nbspPattern, " ");
-}
-
-// find the link, by going over all links on the page
-async function findByLink(page, ElementString) {
-  const links = await page.$$('button')
-  for (var i = 0; i < links.length; i++) {
-    let valueHandle = await links[i].getProperty('innerText');
-    let linkText = await valueHandle.jsonValue();
-    const text = getText(linkText);
-    if (ElementString == text) {
-      console.log(ElementString);
-      console.log(text);
-      console.log("Found");
-      return links[i];
-    }
-  }
-  return null;
-}
-
-const escapedString = str => {
-  const splitedQuotes = str.replace(/'/g, `', "'", '`);
-  return `concat('${splitedQuotes}', '')`;
-};
-
-const clickByText = async (page, text) => {
-  const escapedText = escapedString(text);
-  const linkHandlers = await page.$x(`//a[contains(text(), ${escapedText})]`);
-
-  if (linkHandlers.length > 0) {
-    await linkHandlers[0].click();
-  } else {
-    throw new Error(`Link not found: ${text}`);
-  }
-};
 
 const getAllBtns = async (page) => {
   await page.waitForSelector('ul li.reusable-search__result-container button');
@@ -60,7 +20,10 @@ const getAllBtns = async (page) => {
       })
     )
   });
-
+  await page.$$eval("[aria-label='Add a note']", btn => btn[0].click())
+  await page.waitForSelector("[placeholder='Ex: We know each other from…']")
+  await page.keyboard.type("let's connect");
+  page.$$eval("[aria-label='Send now']", btn => btn[0].click())
 }
 
 async function gotTo(url) {
@@ -69,8 +32,8 @@ async function gotTo(url) {
   page.setViewport({ width: 1366, height: 768 });
   page.goto(url);
   login(page, email, password);
-  searchFor(page, "recruiter");
-  selectCountry(page, "Brazil").then(() => getAllBtns(page))
+  searchFor(page, "recruiter it");
+  selectCountry(page, "Portland").then(() => getAllBtns(page))
 
 
 }
