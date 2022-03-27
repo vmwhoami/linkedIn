8378cb2dfc39ada4dea9cb7840052a622aa1127a
vmwhoami <vitalie.m@travod.com>
2022-03-27 19:13:26 +0300

    Select the conect button make the connection need to fix on skipping buttons that are already connections

--- dist/makeConnections.js
+++ dist/makeConnections.js
@@ -11,33 +11,34 @@ const loopFunc = async (elements_arr, page) => {
     while (elements_arr.length > 0) {
         const selectedElement = elements_arr.shift();
         await selectedElement.click();
+        // waits for the connect button to be visible
+        const selector = await page.waitForSelector('.artdeco-modal__actionbar.ember-view.text-align-right .ml1');
+        console.log(selector);
+        console.log(!selector);
+        if (!selector)
+            continue;
+        await page.click('.artdeco-modal__actionbar.ember-view.text-align-right .ml1');
+        // Place the press connect button here
     }
 };
-const goThroughPages = async (page) => {
+// await for selector
+//document.querySelector('.artdeco-modal__actionbar.ember-view.text-align-right .ml1')
+const goToNextPage = async (page) => {
     const pages = await page.$$('.artdeco-pagination__button.artdeco-pagination__button--next.artdeco-button.artdeco-button--muted.artdeco-button--icon-right artdeco-button--1.artdeco-button--tertiary.ember-view');
-    const pages_arr = [];
-    for (let i = 0; i < pages.length; i++) {
-        pages_arr.push(pages[i]);
-    }
-    await loopFunc(pages_arr, page);
 };
-//  NEXT PAGE BUTTON 
-// .artdeco-pagination__button.artdeco-pagination__button--next.artdeco-button.artdeco-button--muted.artdeco-button--icon-right artdeco-button--1.artdeco-button--tertiary.ember-view
-// We need a loop throught all the pages
-// We a loop throught all the contacts
-// We need to click on the next page when we reach the end of the page
 const connect = async (page) => {
     try {
         await page.waitForTimeout(500);
-        const elementsHendles = await page.evaluateHandle(() => document.querySelectorAll('.entity-result__item .artdeco-button.artdeco-button--2.artdeco-button--secondary.ember-view'));
+        const elementsHendles = await page.evaluateHandle(() => {
+            return document
+                .querySelectorAll('.entity-result__item .artdeco-button.artdeco-button--2.artdeco-button--secondary.ember-view');
+        });
         const elements = await elementsHendles.getProperties();
         const children = [];
         for (const property of elements.values()) {
             const element = property.asElement();
-            if (element)
-                children.push(element);
+            element ? children.push(element) : null;
         }
-        console.log(children);
         await loopFunc(children, page);
     }
     catch (error) {
--- src/makeConnections.ts
+++ src/makeConnections.ts
@@ -1,10 +1,9 @@
 import OptionTypes from './types'
-import urlModifier  from './connect/connectionsUrl';
+import urlModifier from './connect/connectionsUrl';
 
-const makeConnections = async ( page: OptionTypes["page"],
-                                url: OptionTypes["url"], 
-                                connectOptions: OptionTypes["connectOptions"] ): Promise<void> => {
-
+const makeConnections = async (page: OptionTypes["page"],
+  url: OptionTypes["url"],
+  connectOptions: OptionTypes["connectOptions"]): Promise<void> => {
   const modified: string = urlModifier(url, connectOptions);
   await page.goto(modified);
   await connect(page);
@@ -17,44 +16,46 @@ const loopFunc = async (elements_arr: any, page: any) => {
   while (elements_arr.length > 0) {
     const selectedElement = elements_arr.shift()
     await selectedElement.click();
+
+
+    // waits for the connect button to be visible
+    const selector = await page.waitForSelector('.artdeco-modal__actionbar.ember-view.text-align-right .ml1');
+    console.log(selector);
+    console.log(!selector);
     
+    
+    if (!selector) continue;
+    await page.click('.artdeco-modal__actionbar.ember-view.text-align-right .ml1')  
+ 
+   // Place the press connect button here
   }
 }
+// await for selector
+//document.querySelector('.artdeco-modal__actionbar.ember-view.text-align-right .ml1')
 
-const goThroughPages = async (page: any) => {
+const goToNextPage = async (page: any) => {
   const pages: any = await page.$$('.artdeco-pagination__button.artdeco-pagination__button--next.artdeco-button.artdeco-button--muted.artdeco-button--icon-right artdeco-button--1.artdeco-button--tertiary.ember-view');
-  const pages_arr: any = [];
-  for (let i = 0; i < pages.length; i++) {
-    pages_arr.push(pages[i]);
-  }
-  await loopFunc(pages_arr, page);
-}
 
-//  NEXT PAGE BUTTON 
-// .artdeco-pagination__button.artdeco-pagination__button--next.artdeco-button.artdeco-button--muted.artdeco-button--icon-right artdeco-button--1.artdeco-button--tertiary.ember-view
-// We need a loop throught all the pages
-// We a loop throught all the contacts
-// We need to click on the next page when we reach the end of the page
+}
 
 const connect = async (page: any) => {
   try {
     await page.waitForTimeout(500);
-    const elementsHendles = await page.evaluateHandle(() => document.querySelectorAll('.entity-result__item .artdeco-button.artdeco-button--2.artdeco-button--secondary.ember-view'));
+    const elementsHendles = await page.evaluateHandle(() => {
+      return document
+        .querySelectorAll('.entity-result__item .artdeco-button.artdeco-button--2.artdeco-button--secondary.ember-view')
+    });
 
-    const elements:any = await elementsHendles.getProperties();
-    
+    const elements: any = await elementsHendles.getProperties();
+
     const children: any = [];
     for (const property of elements.values()) {
       const element = property.asElement();
-      if (element)
-        children.push(element);
+      element ? children.push(element) : null;
     }
-    console.log(children);
-    
-
     await loopFunc(children, page)
+
   } catch (error) {
     console.log(error);
   }
-
 }
