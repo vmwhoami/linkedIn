da0c1a9ae2f87a749d80969826d6d15a84de9a8d
Vitalie <vmwhoami@gmail.com>
2022-04-27 23:11:23 +0300

    Convert nodeList to array

--- dist/connect/connecterMethod.js
+++ dist/connect/connecterMethod.js
@@ -3,8 +3,9 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const ghost_cursor_1 = require("ghost-cursor");
 const connecterMethod = async (elements_arr, page) => {
     const cursor = (0, ghost_cursor_1.createCursor)(page);
-    while (elements_arr.length > 0) {
-        const selectedElement = elements_arr.shift();
+    let nodeListToArray = Array.from(elements_arr);
+    while (nodeListToArray.length > 0) {
+        const selectedElement = nodeListToArray.shift();
         await selectedElement.click();
         await page.waitForSelector('.artdeco-modal__actionbar.ember-view.text-align-right .ml1');
         const checkObject = await page.waitForFunction(() => {
@@ -14,6 +15,7 @@ const connecterMethod = async (elements_arr, page) => {
             }
         });
         if (checkObject) {
+            // think on changing the connect button it might be the case that it is reinserting the button into the array
             // selectedElement.remove()
             console.log("this is !!! ", checkObject);
             // console.log(elements_arr);
--- src/connect/connecterMethod.ts
+++ src/connect/connecterMethod.ts
@@ -3,9 +3,9 @@ import { createCursor } from "ghost-cursor";
 const connecterMethod = async (elements_arr: any, page: any) => {
   const cursor = createCursor(page);
 
-
-  while (elements_arr.length > 0) {
-    const selectedElement = elements_arr.shift()
+ let nodeListToArray = Array.from(elements_arr) 
+  while (nodeListToArray.length > 0) {
+    const selectedElement = nodeListToArray.shift()
 
     await selectedElement.click();
     await page.waitForSelector('.artdeco-modal__actionbar.ember-view.text-align-right .ml1');
