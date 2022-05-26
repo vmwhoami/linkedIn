fe2bfe7faa46b67d3f3cceb8a23d22c34d4b2f9e
vmwhoami <vitalie.m@travod.com>
2022-05-26 23:17:07 +0300

    switch to for await of function

--- dist/connect/connectorMethod.js
+++ dist/connect/connectorMethod.js
@@ -3,8 +3,9 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const ghost_cursor_1 = require("ghost-cursor");
 const connecterMethod = async (elements_arr, page) => {
     const cursor = (0, ghost_cursor_1.createCursor)(page);
-    elements_arr.forEach(async (selectedElement, index) => {
-        await selectedElement.click();
+    // selectedElement type object
+    for await (const selectedElement of elements_arr) {
+        selectedElement.click();
         if (await page.$('.artdeco-button.artdeco-button--2.artdeco-button--primary.artdeco-button--disabled.ember-view.ml1') !== null) {
             await page.waitForSelector('.artdeco-modal__dismiss.artdeco-button');
             await cursor.click('.artdeco-modal__dismiss.artdeco-button');
@@ -13,7 +14,7 @@ const connecterMethod = async (elements_arr, page) => {
             await page.waitForSelector('.artdeco-modal__actionbar.ember-view.text-align-right .ml1');
             await cursor.click('.artdeco-modal__actionbar.ember-view.text-align-right .ml1');
         }
-    });
+    }
     // while (elements_arr.length > 0) {
     //   const selectedElement = elements_arr.shift();
     //   await selectedElement.click();
--- src/connect/connectorMethod.ts
+++ src/connect/connectorMethod.ts
@@ -3,18 +3,19 @@ import { createCursor } from "ghost-cursor";
 const connecterMethod = async (elements_arr: any, page: any) => {
   const cursor = createCursor(page);
 
-  elements_arr.forEach(async (selectedElement, index) => { 
-    await selectedElement.click();
+  // selectedElement type object
 
-    if (await page.$('.artdeco-button.artdeco-button--2.artdeco-button--primary.artdeco-button--disabled.ember-view.ml1') !== null) {
-      await page.waitForSelector('.artdeco-modal__dismiss.artdeco-button');
-      await cursor.click('.artdeco-modal__dismiss.artdeco-button');
-    } else {
-      await page.waitForSelector('.artdeco-modal__actionbar.ember-view.text-align-right .ml1');
-      await cursor.click('.artdeco-modal__actionbar.ember-view.text-align-right .ml1');
-    }
-
-   } );
+  for await(const selectedElement of elements_arr) {
+      selectedElement.click();
+      if (await page.$('.artdeco-button.artdeco-button--2.artdeco-button--primary.artdeco-button--disabled.ember-view.ml1') !== null) {
+        await page.waitForSelector('.artdeco-modal__dismiss.artdeco-button');
+        await cursor.click('.artdeco-modal__dismiss.artdeco-button');
+      } else {
+        await page.waitForSelector('.artdeco-modal__actionbar.ember-view.text-align-right .ml1');
+        await cursor.click('.artdeco-modal__actionbar.ember-view.text-align-right .ml1');
+      }
+  }
+ 
 
   // while (elements_arr.length > 0) {
   //   const selectedElement = elements_arr.shift();
